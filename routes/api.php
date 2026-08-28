e21e15aaa938750e04efa74d0af0b18ae9ba6206
Jorge Monteiro <dev.henrmont@gmail.com>
2026-08-28 14:53:19 -0400

    (hml) final

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,10 @@ use App\Http\Controllers\StorageController;
 use App\Http\Middleware\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::group(['middleware' => 'api'], function ($router) {
-
-    Route::get('download/{module}/{archive}', [StorageController::class, 'download'])->middleware(Auth::class);
-
-});
+Route::middleware(['api', Auth::class])
+    ->prefix('storage')
+    ->name('storage.')
+    ->controller(StorageController::class)
+    ->group(function () {
+        Route::get('download/{module}/{archive}', 'download')->name('download');
+    });
